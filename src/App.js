69d055950a00dfce3052a24fe7a9d70091b4c8e0
arhab07 <arhab07ah@gmail.com>
2022-04-17 15:05:45 +0530

    deploy my react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react app</h1>
+      <h2>Author: Wildan</h2>
+    </div>
+  );
+}
+
+export default App;
